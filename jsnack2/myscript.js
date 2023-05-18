ee53d25eeea3267d2eeed8be1b6ccdc0017eb083
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-18 17:35:26 +0200

    jsnack2 bonus - input, button & output page print

--- jsnack2/myscript.js
+++ jsnack2/myscript.js
@@ -11,20 +11,29 @@ const guestList = [
     "Marie Schrader"
 ]
 
-let authorization = false;
-const userName = prompt("Inserisci il tuo nome");
-const userLastName = prompt("Inserisci il tuo cognome");
-let userAuthorized;
+const confirmButton = document.querySelector('button');
+confirmButton.addEventListener('click',
+    function() {
+        let authorization = false;
+        const output = document.getElementById('output');
+        const userName = document.getElementById('guestName').value;
+        const userLastName = document.getElementById('guestLastName').value;
+        let userAuthorized;
 
-for (let i = 0; i < guestList.length; i++) {
-    if (userName + " " + userLastName === guestList[i]) {
-        userAuthorized = (userName + " " + userLastName);
-        authorization = true;
-    }
-}
-
-if (authorization === true) {
-    console.log("Congratulazioni " + userAuthorized + ", sei in lista e puoi partecipare alla festa.");
-} else {
-    console.log("Ci dispiace " + userName + " " + userLastName + ", purtroppo non sei in lista e non puoi partecipare alla festa.")
-}
+        if (userName.length === 0 || userLastName.length === 0) {
+            output.innerHTML = "I campi non possono essere vuoti.";
+        } else {
+            for (let i = 0; i < guestList.length; i++) {
+                if (userName + " " + userLastName === guestList[i]) {
+                    userAuthorized = (userName + " " + userLastName);
+                    authorization = true;
+                }
+            }
+        
+            if (authorization === true) {
+                output.innerHTML = "Congratulazioni " + userAuthorized + ", sei in lista e puoi partecipare alla festa.";
+            } else {
+                output.innerHTML = "Ci dispiace " + userName + " " + userLastName + ", purtroppo non sei in lista e non puoi partecipare alla festa.";
+            }
+        }
+    })
