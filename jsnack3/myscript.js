1dd68f9c0d43bc6d3d1c56e482c1d7bba222cf09
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-18 18:33:57 +0200

    add button in jsnack3, you can retry more times

--- jsnack3/myscript.js
+++ jsnack3/myscript.js
@@ -1,23 +1,30 @@
 /* Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. */
 
-const oddNumbers = [];
-const oddNumbersList = document.querySelector('div.container');
+const startButton = document.querySelector('button');
+const oddNumbersList = document.querySelector('div.results');
 
-for (let i = 1; i <= 6; i++) {
-    let userNumber = parseInt(prompt("Inserisci il " + i + "° numero"));
-    if (isNaN(userNumber)) {
-        oddNumbersList.innerHTML += "<p> Il " + i + "° numero inserito non è valido.</p>";
-        userNumber = 0;
-    } else if ((userNumber % 2) != 0) {
-        oddNumbersList.innerHTML += "<p> Il " + i + "° numero inserito è " + userNumber + ".</p>";
-        oddNumbers.push(userNumber);
-    } else {
-        oddNumbersList.innerHTML += "<p> Il " + i + "° numero inserito è " + userNumber + ".</p>";
+startButton.addEventListener('click',
+    function () {
+        const oddNumbers = [];
+        oddNumbersList.innerHTML = " ";
+
+        for (let i = 1; i <= 6; i++) {
+            let userNumber = parseInt(prompt("Inserisci il " + i + "° numero"));
+            if (isNaN(userNumber)) {
+                oddNumbersList.innerHTML += "<p> Il " + i + "° numero inserito non è valido.</p>";
+                userNumber = 0;
+            } else if ((userNumber % 2) != 0) {
+                oddNumbersList.innerHTML += "<p> Il " + i + "° numero inserito è " + userNumber + ".</p>";
+                oddNumbers.push(userNumber);
+            } else {
+                oddNumbersList.innerHTML += "<p> Il " + i + "° numero inserito è " + userNumber + ".</p>";
+            }
+        }
+        
+        if (oddNumbers.length === 0) {
+            oddNumbersList.innerHTML += "<p> Non sono stati inseriti numeri dispari, l'array è vuoto.</p>";
+        } else {
+            oddNumbersList.innerHTML += "<p> Il contenuto dell'array è: " + oddNumbers + "</p>";
+        }
     }
-}
-
-if (oddNumbers.length === 0) {
-    oddNumbersList.innerHTML += "<p> Non sono stati inseriti numeri dispari, l'array è vuoto.</p>";
-} else {
-    oddNumbersList.innerHTML += "<p> Il contenuto dell'array è: " + oddNumbers + "</p>";
-}
+)
