b35c798347d97efa09aae1873d54f9fceec8b4d4
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-18 18:26:33 +0200

    add button in jsnack1, you can retry more times

--- jsnack1/myscript.js
+++ jsnack1/myscript.js
@@ -1,17 +1,25 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. */
 
-let totalNumber = 0;
-const numberList = document.querySelector('div.container');
+const startButton = document.querySelector('button');
+let numberList = document.querySelector('div.results');
 
-for (let i = 1; i <= 10; i++) {
-    let userNumber = parseInt(prompt("Inserisci il " + i + "° numero"));
-    if (isNaN(userNumber) || (userNumber < 0)) {
-        numberList.innerHTML += "<p> Il " + i + "° numero inserito non è valido.</p>";
-        userNumber = 0;
-    } else {
-        numberList.innerHTML += "<p> Il " + i + "° numero inserito è " + userNumber + ".</p>";
+startButton.addEventListener('click',
+    function () {
+        numberList.innerHTML = " ";
+
+        let totalNumber = 0;
+
+        for (let i = 1; i <= 10; i++) {
+            let userNumber = parseInt(prompt("Inserisci il " + i + "° numero"));
+            if (isNaN(userNumber) || (userNumber < 0)) {
+                numberList.innerHTML += "<p> Il " + i + "° numero inserito non è valido.</p>";
+                userNumber = 0;
+            } else {
+                numberList.innerHTML += "<p> Il " + i + "° numero inserito è " + userNumber + ".</p>";
+            }
+            totalNumber += userNumber;
+        }
+    
+        numberList.innerHTML += "<p> La somma dei numeri inseriti è " + totalNumber + ".</p>";
     }
-    totalNumber += userNumber;
-}
-
-numberList.innerHTML += "<p> La somma dei numeri inseriti è " + totalNumber + ".</p>";
+)
